9c55c5f4cd32ec1c40c9ed99fbb3c85b86bc37b8
ralo <leona8464@gmail.com>
2022-10-04 18:47:28 +0900

    #2.2 Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -22,13 +16,17 @@ const Text = styled.h1`
   color: white;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor="teal">
         <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      </Box>
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
