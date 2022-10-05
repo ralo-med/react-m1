196895382c9d781c9b62032b5e6528cf75bb86a2
ralo <leona8464@gmail.com>
2022-10-06 02:02:05 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -2,56 +2,23 @@ import React from "react";
 // eslint-disable-next-line
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotateAniamtion = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;}
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotateAniamtion} 1s linear;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji} {
-    &:hover {
-      font-size: 60px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">ㅇ</Emoji>
-      </Box>
-      <Emoji as="p">X</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
