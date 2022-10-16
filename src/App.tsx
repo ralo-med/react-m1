c58b6d90d5f36f3a711728c10f0cd5c8dea215f6
ralo <leona8464@gmail.com>
2022-10-16 15:59:31 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.div`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log("hello", value);
-    };
-  };
   return (
     <div>
-      <form>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
+      <Container>
+        <H1>Protected</H1>
+      </Container>
     </div>
   );
 }
